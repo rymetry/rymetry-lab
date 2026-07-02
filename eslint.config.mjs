b913e9f7d1c4be4cbc63f0c91faaff11acf2b8ba
rymetry <rymetry88@gmail.com>
2026-07-03 01:18:32 +0900

    ci: fix dependabot auto-merge

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,32 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'storybook-static/**']),
+  globalIgnores([
+    '.next/**',
+    '**/.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'storybook-static/**',
+    '**/storybook-static/**',
+    '.claude/**',
+  ]),
   {
     files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}', '__tests__/**'],
     rules: {
       'no-restricted-imports': 'off',
     },
   },
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs['flat/recommended'],
 ]);
 
 export default eslintConfig;
